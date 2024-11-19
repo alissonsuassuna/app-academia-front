5e117383948dd34a5c5706b85425b008c14f2a4a
RenanKr4t0s <redrums88@gmail.com>
2024-11-19 13:35:54 -0300

    criando pasta backend + limitador no peso e altura

---
+++ Front-End/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import PersonalDataForm from './components/PersonalDataForm'
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-3xl">Jesus te ama</h1>
+      <PersonalDataForm />
+    </>
+  )
+}
+
+export default App
